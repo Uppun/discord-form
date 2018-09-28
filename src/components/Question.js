97603a769b126a5db58169ba55d6b6d4de46ee88
Uppun <TheSuperBro@gmail.com>
2018-09-28 06:24:42 -0400

    did some work on questiontypes

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import FormActions from '../actions/FormActions';
+import QuestionTypes from '../Assets/QuestionTypes';
 
-export default class QuestionObject extends Component {
-    
+export default class ShortQuestionObject extends Component {
+    questionRef = React.createRef();
+    render() {
+        const {form, title} = this.props;
+        return (
+            <div className='short-question-object'>
+                <input type='text' ref={this.titleRef} className='question-entry' defaultValue={question} onChange={this.handleQuestionChange} />
+                <input type='text' className='short-answer' value='Short answer text' />
+            </div>
+        )
+    }
 }
