08a16e4ac0ce3bc36e42920e9427e69c5ce858d8
Uppun <TheSuperBro@gmail.com>
2018-12-18 07:18:08 -0500

    custom dropdowns are here

--- src/viewpage/Questions/SurveyTypes/SurveyDropDown.js
+++ src/viewpage/Questions/SurveyTypes/SurveyDropDown.js
@@ -3,6 +3,44 @@ import '../../../Assets/Forms.css';
 import {Container} from 'flux/utils';
 import AnswerStore from '../../../stores/AnswerStore';
 import FormActions from '../../../actions/FormActions';
+import Select from 'react-select';
+
+const dot = () => ({
+    alignItems: "center",
+    display: "flex",
+    color: "white",
+    width: 200,
+  });
+
+const selectStyle = {
+    control: styles => ({
+        ...styles, 
+        backgroundColor: '#23272A',
+    }),
+    option: (styles, { data, isDisabled, isFocused, isSelected}) => {
+        return {
+            ...styles,
+            backgroundColor: isDisabled
+                ? null
+                : isSelected
+                    ? '#7289DA'
+                    : isFocused
+                        ? '#7289DA'
+                        : '#23272A',
+            color: 'white',
+            cursor: isDisabled ? 'not-allowed' : 'default'
+        };
+    },
+    input: styles => ({ ...styles, ...dot() }),
+    placeholder: styles => ({ ...styles, ...dot() }),
+    singleValue: (styles, { data }) => ({ ...styles, ...dot() }),
+    menuList: styles => {
+        return {
+            ...styles,
+            backgroundColor: '#23272A',
+        }
+    }
+}
 
 class DropDown extends Component {
     static getStores() {
@@ -16,23 +54,35 @@ class DropDown extends Component {
     }
 
     handleChange = (event) => {
-        FormActions.updateAnswer(this.props.id, event.target.value);
+        FormActions.updateAnswer(this.props.id, event.value);
     }
 
     render() {
         const {options, id} = this.props;
         const {AnswersMap} = this.state;
-        const value = AnswersMap.get(id.toString());
+        const currentVal = AnswersMap.get(id.toString());
+        const value = currentVal ? 
+            {value: currentVal , label: currentVal}
+            : {value: '', label: 'Select One'};
+        const optionsArray = [];
+
+        for (const option of options) {
+            optionsArray.push({
+                value: option,
+                label: option,
+            });
+        }
 
         return(
             <div className='survey-dropdown-choice-options'>
-                <select name={id} value={value} className='survey-select' onChange={this.handleChange} >
-                    <option value='Select one'>Select one</option> 
-                    {options.map((option, index) => {
-                        return(
-                            <option value={option} key={index}>{option}</option>
-                        )})}
-                </select>
+                <Select
+                    value={value}
+                    onChange={this.handleChange}
+                    label='Single select'
+                    options={optionsArray}
+                    styles={selectStyle}
+                />
+
             </div>
         )
     }
